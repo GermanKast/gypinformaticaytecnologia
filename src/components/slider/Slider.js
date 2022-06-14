467987934a967bbe35ad1f013ddad1b6654054c5
German <german.casta.rojas@gmail.com>
2022-06-14 15:31:33 -0500

    Update slider - finalizacion slider basico

--- src/components/slider/Slider.js
+++ src/components/slider/Slider.js
@@ -4,27 +4,46 @@ import SliderImgCard from "./SliderImgCard.js";
 import products from "../data/products.js";
 
 function Slider(){
-
+    // numero de imagenes que se ocultan hacia la izquierda dentro del slider
     const [index, setIndex] = useState(0);
+    // pausa el movimiento del slider al mouse enter / mouse leave
     const [pause, setPause] = useState(false);
-    const [items, setItems] = useState(6);//cantidad de items a mostrar
+    //cantidad de items a mostrar dependiendo el ancho de pantalla
+    const [items, setItems] = useState(6);
+
+    const updateItems = () => {
+        let wScreen = window.screen.width;
+        let itemsToShow = 1;
+        if(wScreen >= 1300){
+            itemsToShow = 6;
+        }else if(wScreen >= 1130 && wScreen <= 1299){
+            itemsToShow = 5;
+        }else if(wScreen >= 930 && wScreen <= 1129){
+            itemsToShow = 4;
+        }else if(wScreen >= 730 && wScreen <= 929){
+            itemsToShow = 3;
+        }else if(wScreen >= 530 && wScreen <= 729){
+            itemsToShow = 2;
+        }
+        setItems(itemsToShow);
+    };
 
     const updateIndex = (newIndex) => {
+        
+        updateItems();
         if(newIndex < 0){
             newIndex = products.length - 1;
-        }else if(newIndex >= products.length - items){
+        }else if(newIndex > products.length - items){
             newIndex = 0;
         }
-
         setIndex(newIndex);
+        
     };
 
     useEffect(()=>{
         const interval = setInterval(()=>{
             if(!pause) updateIndex(index+1);
-        }, 3000);
-
-        const updateItems = () => {};
+        }, 2500);
 
         return () => {
             if(interval){
