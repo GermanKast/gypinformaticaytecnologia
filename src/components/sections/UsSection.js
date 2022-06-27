269881603661cac7c2feb5edb70e65d03bf6072d
German <german.casta.rojas@gmail.com>
2022-06-27 04:56:17 -0500

    Actualizacion seccion sobre nosotros

--- src/components/sections/UsSection.js
+++ src/components/sections/UsSection.js
@@ -7,9 +7,9 @@ function UsSection(){
             <img className="us-image" src={require("../../imgs/us/diseño-web-caricatura-fondo-oscuro-short.jpg")} alt="" />
             <div className="us-text">
                 <h2>Sobre Nosotros</h2>
-                <p>Somos un emprendimiento Ibaguereño de dos hermanos con amplios conocimientos y experiencia en el sector publicitario, tecnico y digital.</p>
-                <p>Hemos notado que hoy en dia aun existen muchas personas y empresas con dificultad para usar las nuevas tecnologias debido a altos costos o desconocimiento.</p>
-                <p>Por lo tanto nuestra mision se ha convertido en ayudar a todo quien lo necesite a acceder a las nuevas herramientas digitales.</p>
+                <p>Somos un emprendimiento Ibaguereño radicado en la ciudad de Ibagué Tolima desde 2019, contamos con amplios conocimientos y experiencia en el sector publicitario, tecnico y digital.</p>
+                <br />
+                <p>Nuestra mision es ayudar a todo quien lo necesite a acceder a las nuevas herramientas digitales.</p>
             </div>
         </div>
     );
