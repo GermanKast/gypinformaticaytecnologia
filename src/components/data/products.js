dd6b466668cc92ac94ed11493b875a47509e7249
German <german.casta.rojas@gmail.com>
2022-06-27 17:17:00 -0500

    Actualizacion lista de productos, final desarrollo antes de produccion

--- src/components/data/products.js
+++ src/components/data/products.js
@@ -25,67 +25,137 @@ const products = [
         img:"cable-usb-tipo-c",
         name:"Cable USB - Tipo C",
         price:"15000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Spigen",
+                        "Capacidad: Alta 5V / 5A",
+                        "Largo: 1.5 metros",
+                        "Entrada: Micro USB Tipo C"
+                    ]
     },
     {
         img:"pila-tipo-boton",
-        name:"Pila Tipo Botón",
+        name:"Unidad Pila Tipo Botón",
         price:"5000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Sony CR2032",
+                        "Capacidad: Standard 3V / 235 mAh",
+                        "Dimensiones: 20 mm de diametro y 3,1 mm de alto",
+                        "Uso en: Relojes, Juguetes, BIOS Computadores"
+                    ]
     },
     {
         img:"usb-8gb",
         name:"Memoria USB 8Gb",
         price:"12000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Kingston",
+                        "Capacidad: 8 Gb",
+                        "Tecnología: USB 2.0",
+                        "Presentación: Encapsulado metalico",
+                        "Dimensiones: 20mm de ancho, 44.9mm de alto y 9.2mm de profundidad"
+                    ]
     },
     {
         img:"usb-16gb",
         name:"Memoria USB 16Gb",
         price:"18000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Kingston",
+                        "Capacidad: 16 Gb",
+                        "Tecnología: USB 2.0",
+                        "Presentación: Encapsulado metalico",
+                        "Dimensiones: 20mm de ancho, 44.9mm de alto y 9.2mm de profundidad"
+                    ]
     },
     {
         img:"usb-32gb",
         name:"Memoria USB 32Gb",
         price:"22000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Kingston",
+                        "Capacidad: 32 Gb",
+                        "Tecnología: USB 2.0",
+                        "Presentación: Encapsulado metalico",
+                        "Dimensiones: 20mm de ancho, 44.9mm de alto y 9.2mm de profundidad"
+                    ]
     },
     {
         img:"audifonos",
-        name:"Audifonos Alambricos",
+        name:"Audifonos Estereo",
         price:"10000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Unipha D-21",
+                        "Conector: Jack 3.5 mm",
+                        "Tecnología: Hi-Fi Audio Extra Bass",
+                        "Color: Blanco",
+                        "largo: 1 metro",
+                        "Tipo: Intrauriculares"
+                    ]
     },
     {
         img:"cargador-blanco",
         name:"Cargador USB 3A",
         price:"15000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Ormosia 3.4A",
+                        "Entrada: 100-240 V / 50-60 hz ",
+                        "Salida: 5V / 3.4A",
+                        "Carga Rapida: Si",
+                        "Color: Blanco",
+                        "Incluye cable Micro-USB de 1.5 metros",
+                    ]
     },
     {
         img:"cargador-rojo",
         name:"Cargador USB",
         price:"10000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Fly Energy 2.1A",
+                        "Entrada: 100-240 V / 50-60 hz ",
+                        "Salida: 5V / 2.1A",
+                        "Carga Rapida: No",
+                        "Conector USB: 2",
+                        "Color: Blanco",
+                        "Incluye cable Micro-USB de 1.5 metros",
+                    ]
     },
     {
         img:"cargador-universal",
         name:"Cargador Universal",
         price:"13000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Cargador universal para baterías planas de Litio-ion",
+                        "Referencia: C-U LCD",
+                        "Entrada: CA - 120 V / 50-60 hz",
+                        "Salida: CD - 3,7 a 8,4 V / 2A",
+                        "Carga Rapida: No",
+                        "Incluye pantalla LCD indicativa de carga",
+                    ]
     },
     {
         img:"mouse",
         name:"Mouse Óptico",
         price:"12000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: Jedel CP70",
+                        "Conector: USB",
+                        "Lector: optico",
+                        "Gamer: No",
+                        "Largo cable: 1.5 metros",
+                    ]
     },
     {
         img:"teclado",
         name:"Teclado Multimedia",
         price:"30000",
-        description: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."]
+        description: [  "Especificaciones:",
+                        "Referencia: J&R TMJR-009",
+                        "Conector: USB",
+                        "Gamer: No",
+                        "Teclas de acceso rapido: Si",
+                        "Largo cable: 1 metro",
+                        "Teclas de juego resaltadas"
+                    ]
     }
 ]
 
